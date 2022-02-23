43af90e9bc5d34e14b8bd82a00b96f8afde57f52
Johan Andrès Sabino Castro <jasc9494@gmail.com>
2022-02-23 17:48:04 -0500

    Project update 230222

--- questions/get/v1/internal/ctx/handler.go
+++ questions/get/v1/internal/ctx/handler.go
@@ -1,5 +1,15 @@
 package ctx
 
 import (
-	_ "bytes"
+	"github.com/sirupsen/logrus"
 )
+
+type questionRepositoryInterface interface {
+	questionsRepository()
+}
+
+// Handler main lambda's structure
+type Handler struct {
+	questionsRepository questionRepositoryInterface
+	logger              *logrus.Logger
+}
